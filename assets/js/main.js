c21f13063357b7f101d75eb182f18dd9b9eb51af
remolalata <lalata.remo@gmail.com>
2017-06-22 12:17:21 +0800

    edited by remo

--- assets/js/main.js
+++ assets/js/main.js
@@ -368,14 +368,16 @@ $(document).ready(function () {
     });
 
     $('a[href="#myModalTab1-2"]').on('shown.bs.tab', function () {
-      $('#myModal2b').html('立即注册');
+      $('#myModal2b_registration').css('display', 'block');
+      $('#myModal2b_login').css('display', 'none');
       $('#myModalTab li:nth-child(1)').css('top', '210px');
       $('#myModalTab li:nth-child(1)').css('display', 'block');
       $('#myModalTab li:nth-child(2)').css('display', 'none');
     });
 
     $('a[href="#myModalTab1-2"]').on('hidden.bs.tab', function () {
-      $('#myModal2b').html('註冊賬號');
+      $('#myModal2b_registration').css('display', 'none');
+      $('#myModal2b_login').css('display', 'block');
       $('#myModalTab li:nth-child(1)').css('top', '150px');
       $('#myModalTab li:nth-child(1)').css('display', 'none');
       $('#myModalTab li:nth-child(2)').css('display', 'block');
